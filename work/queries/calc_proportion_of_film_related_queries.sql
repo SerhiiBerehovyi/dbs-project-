8ef062f54242dc33b960470bf8b3eb3ee4f48b5d
matakiba <matakiba@users.noreply.github.com>
2024-07-09 17:07:29 +0200

    Added Abgabe 2 Keynote, added queries, modified import_locations.sql

---
+++ work/queries/calc_proportion_of_film_related_queries.sql
@@ -0,0 +1,47 @@
+with FilmRelatedQueries as (
+    select 
+		aq.user_id, aq.id
+    from 
+		aol_query aq
+    join plot_theme as pt 
+		on aq.text like '%' || lower(pt.name) || '%'
+),
+
+UsersWithFilmRelatedQueries as (
+    select distinct 
+		user_id
+    from 
+		FilmRelatedQueries
+),
+
+TotalQueriesCount as (
+    select 
+		user_id, 
+		count(*) as total_queries
+    from 
+		aol_query
+    where 
+		user_id in 
+			(select user_id from UsersWithFilmRelatedQueries)
+    group by 
+		user_id
+),
+
+FilmRelatedQueriesCount as (
+    select 
+		user_id, 
+		count(*) as film_queries
+    from 
+		FilmRelatedQueries
+    group by 
+		user_id
+)
+
+select 
+	avg(cast(related_cnt.film_queries as FLOAT) / total_count.total_queries) 
+		as proportion
+from 
+	TotalQueriesCount as total_count
+join 
+	FilmRelatedQueriesCount as related_cnt 
+	on total_count.user_id = related_cnt.user_id;
